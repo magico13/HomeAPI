19f490a36692c881bfd75fdeacc0bb1f3749fa0b
magico13 <magico1313@gmail.com>
2018-07-05 17:52:29 -0400

    Move wifi info to a .private file as well.

--- HomeAPI/Pages/Index.cshtml.cs
+++ HomeAPI/Pages/Index.cshtml.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HomeAPI.Pages
 {
     public class IndexModel : PageModel
     {
+        public string WifiInfo { get; set; }
         public void OnGet()
         {
-
+            if (System.IO.File.Exists("Configuration/wifi.private"))
+            {
+                string file = System.IO.File.ReadAllText("Configuration/wifi.private");
+                JObject obj = JsonConvert.DeserializeObject<JObject>(file);
+                WifiInfo = $"{obj["SSID"]} / {obj["Key"]}";
+            }
         }
     }
 }
