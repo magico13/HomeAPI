938d3ee7089a0c008e0de9c431f702353bb149e3
magico13 <magico1313@gmail.com>
2018-07-08 17:18:03 -0400

    Show the lights as clickable tiles instead of a table

--- HomeAPI/Pages/Lights.cshtml.cs
+++ HomeAPI/Pages/Lights.cshtml.cs
@@ -27,44 +27,5 @@ namespace HomeAPI.Pages
             Lights = await _hueConnector.GetLightsAsync();
             Lights = Lights.OrderBy(l => l.Name).ToList();
         }
-
-        public async Task<IActionResult> OnPostUpdateLightAsync(long id, int index, bool state)
-        {
-            int actualIndex = -1;
-            if (Request.Form.TryGetValue("i", out StringValues indexVals))
-            {
-                //find i == index, that's our actual index
-                for (int i=0; i<indexVals.Count; i++)
-                {
-                    if (int.TryParse(indexVals[i], out int tmpIndex))
-                    {
-                        if (tmpIndex == index)
-                        {
-                            actualIndex = i;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (actualIndex < 0)
-            {
-                return NotFound();
-            }
-
-            int brightness = 100;
-            if (Request.Form.TryGetValue("briScaled", out StringValues vals))
-            {
-                if (!int.TryParse(vals[actualIndex], out brightness))
-                {
-                    brightness = 100;
-                }
-            }
-
-            int newBrightness = (int)Math.Round(brightness * 2.54);
-            newBrightness = Math.Min(254, Math.Max(1, newBrightness)); //restrict to between 0 and 254
-            await _hueConnector.UpdateLightAsync(id, new HueLight.BulbState() { On = state, Bri = newBrightness });
-            return RedirectToPage();
-        }
-        
     }
 }
